05c5ebe944c73f48aa23f99e5178b67d2c7ba0a6
ionif <aionkov@gmail.com>
2020-07-17 21:41:13 -0500

    updated to get working offline

--- src/index.js
+++ src/index.js
@@ -15,8 +15,6 @@
  * =============================================================================
  */
 
-import * as handpose from "@tensorflow-models/handpose";
-
 function isMobile() {
   const isAndroid = /Android/i.test(navigator.userAgent);
   const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
